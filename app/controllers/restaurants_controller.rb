bd547031954baee30c762fd417851e39be65600c
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2019-04-30 10:08:02 -0300

    search restaurant using ransack

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,13 +11,18 @@ class RestaurantsController < ApplicationController
   end
 
   def search
+    @restaurants = Restaurant.search(
+      name_or_description_cont: params[:q]
+      ).result
+    @restaurants = @restaurants.near(params[:city]) if params[:city]
+    render json: @restaurants
   end
 
   private
     def set_restaurant
       @restaurant = Restaurant.find_by(id: params[:id])
     end
-    
+
     def filter_by_category
       @restaurants = @restaurants.select do |r|
         r.category.title == params[:category]
